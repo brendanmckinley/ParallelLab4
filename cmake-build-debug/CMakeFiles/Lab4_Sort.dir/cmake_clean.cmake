ede3fb7794d58d19a570f64e0930d270aa18b9de
Brendan <brendan.mckinley@eagles.oc.edu>
2019-12-07 16:44:20 -0600

    Add non-parallelized code

---
+++ cmake-build-debug/CMakeFiles/Lab4_Sort.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab4_Sort.dir/main.c.o"
+  "Lab4_Sort"
+  "Lab4_Sort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Lab4_Sort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,95 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+#include <time.h>
+#include <sys/time.h>
+#include <memory.h>
+
+static const long Num_To_Sort = 10000;
+
+/* Function to print an array */
+void printArray(int* arr)
+{
+    int i;
+    for (i = 0; i < Num_To_Sort; i++)
+        printf("%i\n", arr[i]);
+}
+
+// Swap function
+// Obtained from https://www.geeksforgeeks.org/bubble-sort/
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// Sequential version of your sort
+// If you're implementing the PSRS algorithm, you may ignore this section
+void sort_s(int *arr) {
+    int i, j;
+    for (i = 0; i < Num_To_Sort-1; i++) {
+        // Last i elements are already in place
+        for (j = 0; j < Num_To_Sort - i - 1; j++) {
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+        }
+    }
+}
+
+// Parallel version of your sort
+void sort_p(int *arr) {
+    int i, j;
+    #pragma omp parallel num_threads(omp_get_max_threads())
+    for (i = 0; i < Num_To_Sort-1; i++) {
+        // Last i elements are already in place
+        for (j = 0; j < Num_To_Sort - i - 1; j++) {
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+        }
+    }
+}
+
+int main() {
+    int *arr_s = malloc(sizeof(int) * Num_To_Sort);
+    long chunk_size = Num_To_Sort / omp_get_max_threads();
+#pragma omp parallel num_threads(omp_get_max_threads())
+    {
+        int p = omp_get_thread_num();
+        unsigned int seed = (unsigned int) time(NULL) + (unsigned int) p;
+        long chunk_start = p * chunk_size;
+        long chunk_end = chunk_start + chunk_size;
+        for (long i = chunk_start; i < chunk_end; i++) {
+            arr_s[i] = rand_r(&seed);
+        }
+    }
+
+    // Copy the array so that the sorting function can operate on it directly.
+    // Note that this doubles the memory usage.
+    // You may wish to test with slightly smaller arrays if you're running out of memory.
+    int *arr_p = malloc(sizeof(int) * Num_To_Sort);
+    memcpy(arr_p, arr_s, sizeof(int) * Num_To_Sort);
+
+    struct timeval start, end;
+
+    printf("Timing sequential...\n");
+    gettimeofday(&start, NULL);
+    sort_s(arr_s);
+    gettimeofday(&end, NULL);
+    printf("Took %f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000);
+
+    //printArray(arr_s);
+
+    free(arr_s);
+
+    printf("Timing parallel...\n");
+    gettimeofday(&start, NULL);
+    sort_p(arr_p);
+    gettimeofday(&end, NULL);
+    printf("Took %f seconds\n\n", end.tv_sec - start.tv_sec + (double) (end.tv_usec - start.tv_usec) / 1000000);
+
+    free(arr_p);
+
+    return 0;
+}
+
